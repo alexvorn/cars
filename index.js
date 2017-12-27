74c36fef0220ecb1304adf0ade9969b637ce5d8e
Andrei Starusev <a.starusiev@zajno.com>
2017-12-27 17:16:23 +0200

    cataloge

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+// slick initialize
 $(document).ready(function() {
     $('.your-class').slick();
 });
+// end of slick
+
+const listItems = document.querySelectorAll('#advancing-list li');
+const selectedBtn = document.getElementById('select-button');
+let selectedText = null;
+
+// set click to drop-down elements
+listItems.forEach(item => {
+    item.addEventListener('click', e => {
+        selectedText = e.target.textContent;
+        changeSelectedContent(selectedText);
+    });
+});
+
+document.getElementById('btn-help').addEventListener('click', () => {
+    sendToEmail(null, null, null, selectedText);
+});
+
+function changeSelectedContent(content) {
+    selectedBtn.innerHTML = content;
+}
+
+function sendToEmail(name, phoneNumber, autoType, selectedAccessory) {
+    console.log('selectedAccessory', selectedAccessory);
+    // send(name, phoneNumber, autoType, selectedAccessory); for example
+}
